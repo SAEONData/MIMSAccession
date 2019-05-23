3b9631c3da50f6e7dd7ef12ce8bcea5269a9d1f9
grahamtraas@gmail.com <grahamtraas@gmail.com>
2019-05-23 14:52:45 +0200

    Initial setup of database and 1st endpoint creation. App.properties also hooked up

--- src/main/java/org/saeon/mims/accession/AccessionApplication.java
+++ src/main/java/org/saeon/mims/accession/AccessionApplication.java
@@ -1,5 +1,6 @@
 package org.saeon.mims.accession;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
